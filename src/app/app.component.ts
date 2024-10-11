4ac23b5d3992c2f8a47f99016d345168c700d753
Igor Emanuel <igor17emanuel@gmail.com>
2024-10-11 19:58:35 -0300

    Inicio do projeto e dependencias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'control-finance';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
